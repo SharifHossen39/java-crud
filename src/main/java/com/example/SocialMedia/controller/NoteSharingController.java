27c38165501720aec72d2c4c423195f482151581
SharifHossen39 <sharif.hossen@brainstation-23.com>
2026-09-03 13:29:13 +0600

    DTO pattern implementation

--- src/main/java/com/example/SocialMedia/controller/NoteSharingController.java
+++ src/main/java/com/example/SocialMedia/controller/NoteSharingController.java
@@ -1,6 +1,8 @@
 package com.example.SocialMedia.controller;
 
 import com.example.SocialMedia.entity.NoteSharing;
+import com.example.SocialMedia.payload.request.NoteSharingRequest;
+import com.example.SocialMedia.payload.response.NoteSharingResponse;
 import com.example.SocialMedia.service.NotSharingService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
@@ -13,19 +15,19 @@ public class NoteSharingController {
     private NotSharingService noteSharingService;
 
     @PostMapping("/create")
-    private String create(@RequestBody NoteSharing noteSharing) {
-        noteSharingService.createNote(noteSharing);
+    private String create(@RequestBody NoteSharingRequest noteSharingRequest) {
+        noteSharingService.createNote(noteSharingRequest);
         return "success";
     }
 
     @GetMapping("/byId")
-    private NoteSharing findById(@RequestParam Long id) {
-        NoteSharing note = noteSharingService.findByID(id);
+    private NoteSharingResponse findById(@RequestParam Long id) {
+        NoteSharingResponse note = noteSharingService.findByID(id);
         return note;
     }
 
     @GetMapping("/allInfo")
-    private List<NoteSharing> findallinformation() {
+    private List<NoteSharingResponse> findallinformation() {
         return noteSharingService.findAllInfo();
     }
 
--- src/main/java/com/example/SocialMedia/service/NotSharingService.java
+++ src/main/java/com/example/SocialMedia/service/NotSharingService.java
@@ -1,10 +1,13 @@
 package com.example.SocialMedia.service;
 
 import com.example.SocialMedia.entity.NoteSharing;
+import com.example.SocialMedia.payload.request.NoteSharingRequest;
+import com.example.SocialMedia.payload.response.NoteSharingResponse;
 import com.example.SocialMedia.repository.NoteSharingRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Service
@@ -12,18 +15,49 @@ public class NotSharingService {
     @Autowired
     private NoteSharingRepository noteSharingRepository;
 
-    public String createNote(NoteSharing noteSharing){
+    public String createNote(NoteSharingRequest noteSharingRequest){
+        NoteSharing noteSharing = convertToRequest(noteSharingRequest);
         noteSharingRepository.save(noteSharing);
         return "Note created successfully";
     }
 
-    public NoteSharing findByID(Long Id) {
-        NoteSharing note = noteSharingRepository.findById(Id).get();
-        return note;
+    public NoteSharing convertToRequest(NoteSharingRequest noteSharingRequest) {
+        NoteSharing noteSharing = new NoteSharing();
+        noteSharing.setAuthor(noteSharingRequest.getAuthor());
+        noteSharing.setTitle(noteSharingRequest.getTitle());
+        noteSharing.setDescription(noteSharingRequest.getDescription());
+        noteSharing.setAuthorId(noteSharingRequest.getAuthorId());
+        noteSharing.setAuthorPassword(noteSharingRequest.getAuthorPassword());
+        return noteSharing;
     }
 
-    public List<NoteSharing> findAllInfo() {
-        return noteSharingRepository.findAll();
+    public NoteSharingResponse convertToResponse(NoteSharing noteSharing) {
+        NoteSharingResponse noteSharingResponse = new NoteSharingResponse();
+
+        noteSharingResponse.setAuthor(noteSharing.getAuthor());
+        noteSharingResponse.setTitle(noteSharing.getTitle());
+        noteSharingResponse.setDescription(noteSharing.getDescription());
+
+        return noteSharingResponse;
+    }
+
+    public NoteSharingResponse findByID(Long Id) {
+        NoteSharing note = noteSharingRepository.findById(Id).get();
+        NoteSharingResponse noteSharingResponse = convertToResponse(note);
+        return noteSharingResponse;
+    }
+
+    public List<NoteSharingResponse> findAllInfo() {
+        List<NoteSharing> noteSharings = noteSharingRepository.findAll();
+
+        List<NoteSharingResponse> noteSharingResponses = new ArrayList<>();
+
+        for(NoteSharing noteSharing : noteSharings) {
+            NoteSharingResponse noteSharingResponse = convertToResponse(noteSharing);
+            noteSharingResponses.add(noteSharingResponse);
+        }
+
+        return noteSharingResponses;
     }
 
     public String deleteNote(Long Id) {
